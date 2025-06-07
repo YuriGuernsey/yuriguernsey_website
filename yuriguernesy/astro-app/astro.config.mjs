066f413846e250bb82a99a82d118ef6b6ae87591
Yuri Alves <yurialvesguernsey@outlook.com>
2025-06-07 12:47:24 +0000

    bad idea but may work

--- yuriguernesy/astro-app/astro.config.mjs
+++ yuriguernesy/astro-app/astro.config.mjs
@@ -1,17 +1,17 @@
 // Loading environment variables from .env files
 // https://docs.astro.build/en/guides/configuring-astro/#environment-variables
 import { loadEnv } from "vite";
-const {
-  PUBLIC_SANITY_STUDIO_PROJECT_ID,
-  PUBLIC_SANITY_STUDIO_DATASET,
-  PUBLIC_SANITY_PROJECT_ID,
-  PUBLIC_SANITY_DATASET,
-} = loadEnv(import.meta.env.MODE, process.cwd(), "");
-import { defineConfig } from "astro/config";
+// const {
+//   PUBLIC_SANITY_STUDIO_PROJECT_ID,
+//   PUBLIC_SANITY_STUDIO_DATASET,
+//   PUBLIC_SANITY_PROJECT_ID,
+//   PUBLIC_SANITY_DATASET,
+// } = loadEnv(import.meta.env.MODE, process.cwd(), "");
+// import { defineConfig } from "astro/config";
 
-// Different environments use different variables
-const projectId = PUBLIC_SANITY_STUDIO_PROJECT_ID || PUBLIC_SANITY_PROJECT_ID;
-const dataset = PUBLIC_SANITY_STUDIO_DATASET || PUBLIC_SANITY_DATASET;
+// // Different environments use different variables
+// const projectId = PUBLIC_SANITY_STUDIO_PROJECT_ID || PUBLIC_SANITY_PROJECT_ID;
+// const dataset = PUBLIC_SANITY_STUDIO_DATASET || PUBLIC_SANITY_DATASET;
 
 import sanity from "@sanity/astro";
 import react from "@astrojs/react";
@@ -26,8 +26,8 @@ export default defineConfig({
 }),
   integrations: [
     sanity({
-      projectId,
-      dataset,
+      projectId: 'ek5zr1zv',
+      dataset: 'production',
       // studioBasePath: "/admin",
       useCdn: false,
       // `false` if you want to ensure fresh data
